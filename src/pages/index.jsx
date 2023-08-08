1f97ebbf25ce1a172153460a10aae3a710d028b7
Daniel Abrante <danielabrante@protonmail.com>
2023-08-08 14:02:27 -0300

    Third commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,18 @@
-export default function Home() {
+// Nome, Senha, Idade, Comentário, Selecionar foto, radio, checkbox, button
+import Link from "next/link"
+
+export default function App() {
   return (
     <main>
-      <h1>Oi</h1>
+      <form>
+        <p>
+          <label htmlFor="name">Nome</label>
+          <input type="text" id="name" />
+        </p>
+        <Link href='/home'>
+          <button>Submit</button>
+        </Link>
+      </form>
     </main>
   )
 }
