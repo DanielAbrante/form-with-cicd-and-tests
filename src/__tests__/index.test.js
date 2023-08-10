2177b1e3d4030b09bcf67cb765b3b495d4916037
Daniel Abrante <danielabrante@protonmail.com>
2023-08-10 15:34:39 -0300

    add basic api and consume

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -2,22 +2,7 @@ import { render, screen } from "@testing-library/react"
 import App from "@/pages/_app"
 import "@testing-library/jest-dom"
 
-// import dependencies
-import React from 'react'
-
-// import API mocking utilities from Mock Service Worker
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
-
-// import react-testing methods
-import { render, fireEvent, screen } from '@testing-library/react'
-
-// add custom jest matchers from jest-dom
-import '@testing-library/jest-dom'
-// the component to test
-import Fetch from '../fetch'
-
-// Task - testar describe com e sem "concurrente" para medir performance ou velocidade
+// Task - testar describe com e sem "concurrent" para medir performance ou velocidade
 describe("App", () => {
   it("Should render App page", async () => {
     render(<App />)
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,38 @@
 // Nome, Senha, Idade, Comentário, Selecionar foto, radio, checkbox, button
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    async function getNames() {
+      const data = await fetch("http://localhost:3000/names");
+      const names = await data.json();
+
+      setAuthors(names);
+    }
+
+    getNames();
+  }, [])
+
   return (
     <main>
       <h1>Formulário</h1>
+
+      <br />
+
+      <h2>Autores</h2>
+      <ul>
+        {authors.map(author => <li key={Math.random}>{author}</li>)}
+      </ul>
+
+      <br />
+
       <form>
         <p>
           <label htmlFor="name">Nome</label>
-          <input type="text" id="name" value="Daniel" />
+          <input type="text" id="name" />
         </p>
         <Link href='/home'>
           <button>Submit</button>
